4154353a58bc27229494c587aec1c425acc9663d
dejoyag782 <dejoyag782@gmail.com>
2023-09-05 20:26:31 +0800

    added custom dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,21 +10,32 @@
     <title>{{ config('app.name', 'BISU-TTG') }}</title>
 
     <!-- Fonts -->
-    <link rel="stylesheet" href="{{asset('../welcome_assets/fonts/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('welcome_assets/fonts/font-awesome.min.css')}}">
 
     <!-- Import Styles  -->
     <link rel="stylesheet" href="{{asset('welcome_assets/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('welcome_assets/fonts/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('welcome_assets/css/Animated-CSS-Waves-Background-SVG.css')}}">
+    <link rel="stylesheet" href="{{asset('welcome_assets/css/Contact-Form-Clean.css')}}">
+    <link rel="stylesheet" href="{{asset('welcome_assets/css/dash.css')}}">
+    <link rel="stylesheet" href="{{asset('welcome_assets/css/Data-Table-1.css')}}">
+    <link rel="stylesheet" href="{{asset('welcome_assets/css/Data-Table.css')}}">
     <link rel="stylesheet" href="{{asset('welcome_assets/css/Features-Blue.css')}}">
     <link rel="stylesheet" href="{{asset('welcome_assets/css/Highlight-Phone.css')}}">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
     <link rel="stylesheet" href="{{asset('welcome_assets/css/Login-Form-Clean.css')}}">
     <link rel="stylesheet" href="{{asset('welcome_assets/css/media.css')}}">
+    <link rel="stylesheet" href="{{asset('welcome_assets/css/Modal-Login-form.css')}}">
+    <link rel="stylesheet" href="{{asset('welcome_assets/css/modal.css')}}">
     <link rel="stylesheet" href="{{asset('welcome_assets/css/Navigation-Clean.css')}}">
     <link rel="stylesheet" href="{{asset('welcome_assets/css/Navigation-with-Button.css')}}">
+    <link rel="stylesheet" href="{{asset('welcome_assets/css/Newsletter-Subscription-Form.css')}}">
     <link rel="stylesheet" href="{{asset('welcome_assets/css/styles.css')}}">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/js/app.js'])
 </head>
 <body>
     <div id="app">
@@ -66,11 +77,17 @@
         </div>
     </nav>
 
-        <main class="py-4">
+        <main class="">
             @yield('content')
         </main>
     </div>
     <!-- <script src="{{asset('welcome_assets/bootstrap/js/bootstrap.min.js')}}"></script> -->
     <!-- <script src="{{asset('welcome_assets/js/bs-init.js')}}"></script> -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+    <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script>
+    <script src="{{asset('welcome_assets/js/bs-init.js')}}"></script>
+    <script src="{{asset('welcome_assets/js/mainview.js')}}"></script>
+    <script src="{{asset('welcome_assets/js/modal.js')}}"></script>
 </body>
 </html>
